ee987a72a901d5753e43d0ea5fb3658bcf6e4a79
SergSvet87 <svetlichnij1905@gmail.com>
2022-12-22 14:09:44 +0200

    сhanged the display of films by year

--- src/components/Main/Films/Films.jsx
+++ src/components/Main/Films/Films.jsx
@@ -5,22 +5,24 @@ import { Film } from './Film';
 import styles from './Films.module.scss';
 
 export const Films = ({ movies }) => {
+  const films = movies.filter(
+    (film) => film.summary !== '' && film.medium_cover_image !== '404'
+  );
+
   return (
     <div className="container">
       <ul className={styles.films}>
-        {movies.map((movie) => (
+        {films.map((movie) => (
           <li className={styles.film} key={movie.id}>
-            {movie.medium_cover_image && movie.summary ? (
-              <Film
-                id={movie.id}
-                year={movie.year}
-                rating={movie.rating}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ) : ''}
+            <Film
+              id={movie.id}
+              year={movie.year}
+              rating={movie.rating}
+              title={movie.title}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
           </li>
         ))}
       </ul>
--- src/utils/urls.js
+++ src/utils/urls.js
@@ -7,6 +7,6 @@ export const PATHS = {
 };
 
 
-export const ALL_FILMS_URL = `https://yts.mx/api/v2/list_movies.json?limit=30`
+export const ALL_FILMS_URL = `https://yts.mx/api/v2/list_movies.json?limit=40&sort_by=year`
 
 // ?genre=fantasy&sort_by=year&limit=33&minimum_rating=5.5
