d2836adacf703df86a57289c0db93a1f87dd1a19
SergSvet87 <svetlichnij1905@gmail.com>
2022-12-24 13:01:53 +0200

    changed urls

--- src/const.js
+++ src/const.js
@@ -8,9 +8,12 @@ export const PATHS = {
 };
 
 
-export const ALL_FILMS_URL = `https://yts.mx/api/v2/list_movies.json?page=1&limit=30&sort_by=year`
-export const ALL_FILMS_BY_GENRE_URL = `https://yts.mx/api/v2/list_movies.json?page=1&limit=30&sort_by=year&genre=fantasy`
-export const POPULAR_FILMS_URL = `https://yts.mx/api/v2/movie_suggestions.json?movie_id=10`
-export const BASE_URL = `https://yts.mx/api/v2/list_movies.json`
+export const ALL_FILMS_URL = `https://yts.mx/api/v2/list_movies.json?page=1&limit=30&sort_by=year`  //main
+export const ALL_FILMS_BY_GENRE_URL = `https://yts.mx/api/v2/list_movies.json?page=1&limit=30&sort_by=year&genre=fantasy`  //genre
+export const POPULAR_FILMS_URL = `https://yts.mx/api/v2/movie_suggestions.json?movie_id=10`  //popular
+export const INFO_FILM_URL = 'https://yts.mx/api/v2/movie_details.json?movie_id=10&with_images=true&with_cast=true'  //info film
+
+
+// export const BASE_URL = `https://yts.mx/api/v2/list_movies.json`
 
 // ?genre=fantasy&sort_by=year&limit=33&minimum_rating=5.5
