36d3c4a7cd6ca01a4b9882514a6853d031c8992f
SergSvet87 <svetlichnij1905@gmail.com>
2022-12-25 22:12:28 +0200

    changed const folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { RouterProvider } from 'react-router-dom';
+import { MemoryRouter, RouterProvider } from 'react-router-dom';
 
 import { router } from './router';
 import { Loader } from './components/loader/Loader';
 
 export const App = () => (
-  <RouterProvider router={router} fallbackElement={<Loader />}></RouterProvider>
+  <>
+    <RouterProvider base router={router} fallbackElement={<Loader />}></RouterProvider>
+    <MemoryRouter basename="/App_Movies"></MemoryRouter>
+  </>
 );
