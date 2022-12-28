67e9ca7ee1498f421170311890bb7d449198eebb
SergSvet87 <svetlichnij1905@gmail.com>
2022-12-28 23:09:11 +0200

    change slider

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,23 +1,12 @@
 import { useEffect, useState } from 'react';
 
+import { DEFAULT_SLIDES } from '../../const';
 import { Weather } from '../weather/Weather';
 
 import styles from './Slider.module.scss';
 
-const defaultSlides = [
-  'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg',
-  'https://images.thedirect.com/media/photos/posd1_1.jpg',
-  'https://i.pinimg.com/originals/46/1f/17/461f1790130dd8a619c65cf8ebac949c.jpg',
-  'https://www.komar.de/ru/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/v/d/vd-046-star-wars-official-poster-ep7.jpg',
-  'https://www.komar.de/en/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4113_star_wars_movie_poster_rey_ma.jpg',
-  'https://cdna.artstation.com/p/assets/images/images/049/362/122/large/ramon-ost-2morbius-projetct-04-original.jpg?1652308209',
-  'https://akns-images.eonline.com/eol_images/Entire_Site/2018112/rs_634x939-181202195654-634.captain-marvel.12418.jpg?fit=around%7C634:939&output-quality=90&crop=634:939;center,top',
-  'http://cdn.shopify.com/s/files/1/0037/8008/3782/products/IMG_6203_2_framed2-399939.jpg?v=1611687760',
-  'https://i.pinimg.com/736x/30/dc/6c/30dc6cd7a41fd5e25b5ce9415adec6eb.jpg',
-];
-
 export const Slider = () => {
-  const [slides] = useState(defaultSlides);
+  const [slides] = useState(DEFAULT_SLIDES);
   const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
@@ -28,7 +17,7 @@ export const Slider = () => {
   });
 
   const next = () => {
-    if (currentSlide === defaultSlides.length - 1) {
+    if (currentSlide === slides.length - 1) {
       setCurrentSlide(0);
     } else {
       setCurrentSlide(currentSlide + 1);
@@ -37,7 +26,7 @@ export const Slider = () => {
 
   const prev = () => {
     if (currentSlide === 0) {
-      setCurrentSlide(defaultSlides.length - 1);
+      setCurrentSlide(slides.length - 1);
     } else {
       setCurrentSlide(currentSlide - 1);
     }
--- src/const.js
+++ src/const.js
@@ -10,14 +10,13 @@ export const PATHS = {
   search: (search) => (search ? `&search=${search}` : '&search=:search'),
 };
 
-export const BASE_URL = `https://yts.mx/api/v2/list_movies.json?limit=30&sort_by=year`; //main
+export const BASE_URL = `https://yts.mx/api/v2/list_movies.json?limit=30&sort_by=year`;
 
-export const POPULAR_FILMS_URL = `https://yts.mx/api/v2/movie_suggestions.json?`; //popular
+export const POPULAR_FILMS_URL = `https://yts.mx/api/v2/movie_suggestions.json?`;
 
 export const INFO_FILM_URL =
-  'https://yts.mx/api/v2/movie_details.json?with_images=true&with_cast=true'; //info film
+  'https://yts.mx/api/v2/movie_details.json?with_images=true&with_cast=true&with_images=true&';
 
-// ?genre=fantasy&sort_by=year&limit=33&minimum_rating=5.5
 
 export const PAGES = ['home', 'about', 'team'];
 
@@ -166,4 +165,16 @@ export const TEAM = [
       },
     ],
   },
+];
+
+export const DEFAULT_SLIDES = [
+  'https://www.washingtonpost.com/graphics/2019/entertainment/oscar-nominees-movie-poster-design/img/black-panther-web.jpg',
+  'https://images.thedirect.com/media/photos/posd1_1.jpg',
+  'https://i.pinimg.com/originals/46/1f/17/461f1790130dd8a619c65cf8ebac949c.jpg',
+  'https://www.komar.de/ru/media/catalog/product/cache/7/image/9df78eab33525d08d6e5fb8d27136e95/v/d/vd-046-star-wars-official-poster-ep7.jpg',
+  'https://www.komar.de/en/media/catalog/product/cache/5/image/9df78eab33525d08d6e5fb8d27136e95/4/-/4-4113_star_wars_movie_poster_rey_ma.jpg',
+  'https://cdna.artstation.com/p/assets/images/images/049/362/122/large/ramon-ost-2morbius-projetct-04-original.jpg?1652308209',
+  'https://akns-images.eonline.com/eol_images/Entire_Site/2018112/rs_634x939-181202195654-634.captain-marvel.12418.jpg?fit=around%7C634:939&output-quality=90&crop=634:939;center,top',
+  'http://cdn.shopify.com/s/files/1/0037/8008/3782/products/IMG_6203_2_framed2-399939.jpg?v=1611687760',
+  'https://i.pinimg.com/736x/30/dc/6c/30dc6cd7a41fd5e25b5ce9415adec6eb.jpg',
 ];
